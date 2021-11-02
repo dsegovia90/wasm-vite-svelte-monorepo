f9a98f510160f729cada637e7ab511a763425876
Daniel Segovia <segoarce90@gmail.com>
2021-11-01 20:05:09 -0700

    measure performance of wasm init

--- packages/web/src/main.ts
+++ packages/web/src/main.ts
@@ -2,7 +2,11 @@ import App from './App.svelte'
 import init from 'vite-wasm-functions'
 
 const load = async () => {
+  const startTime = performance.now()
   await init()
+  const endTime = performance.now()
+  console.log(`Call to wasm init took ${endTime - startTime} milliseconds`)
+
 
   const app = new App({
     target: document.getElementById('app')
